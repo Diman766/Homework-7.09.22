c7cc352fdfdce55cfe659fdbc0c790bc85881309
unknown <Dmitiy_yar@mail.ru>
2022-09-07 10:27:15 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координаты точки А (X,Y,Z)");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B (X,Y,Z");
+
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+double C = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+Console.WriteLine(Math.Round(C,2));
